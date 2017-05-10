d517f2027715bd7d38d8abca650b7bff8ec08584
evengyl <dark.evengyl@gmail.com>
2017-05-10 14:03:27 +0200

    correction et mise en place du form pour le change cofig app

--- app/controller/admin_tool/admin_edit_config_app.php
+++ app/controller/admin_tool/admin_edit_config_app.php
@@ -7,21 +7,42 @@ Class admin_edit_config_app extends base_module
 	{		
 		parent::__construct(__CLASS__);
 
-		//var pouvant être modififée du config pour l'app
-		//prefixe, mail, base_sql
-		//tous doivent être dans un fichier disctinct pour la lecture
-		$_get_config = get_config_base();
-		affiche_pre($_get_config);
+		//on recup le contenu sous forme d'objet du jason config
+		$_get_config = $this->get_config_base();
 
+		//operation de modification du JSON recu
+		if(isset($_POST['form__config']) && $_POST['form__config'] == 71414242)
+		{
+			unset($_POST['form__config']);
 
-		$this->get_html_tpl = $this->render_tpl();
+			foreach($_POST as $key_post => $row_post)
+			{
+				if(empty($row_post))
+					unset($_POST[$key_post]);
+			}
+
+			$_new_config = array_merge((array)$_get_config, $_POST);
+
+			$this->push_config_base($_new_config);
+
+			$_get_config = $this->get_config_base();
+		}
+		
+
+		$this->get_html_tpl = $this->assign_var('_config', $_get_config)->render_tpl();
 	}
 
 
     public function get_config_base()
     {
-        $_config = file_get_contents('../app/modele/Config.conf', 'r');
+        $_config = file_get_contents('../app/modele/Config.conf');
 
         return json_decode($_config);
+    }
+
+    public function push_config_base($_get_config_to_push)
+    {
+    	$_get_config_to_push = json_encode($_get_config_to_push);
+    	file_put_contents('../app/modele/Config.conf', $_get_config_to_push);
     }
 }
--- vues/admin.php
+++ vues/admin.php
@@ -11,21 +11,22 @@
 					
 				else if($_GET['action'] == "edit_config_app")
 				{
-					?>__MOD_admin_git_pull__<?
+					?>__MOD_admin_edit_config_app__<?
 				}
 					
 			}
+			else
+			{?>
+				<div class="col-xs-6 col-xs-offset-3">
+					<?=($_SERVER['HTTP_HOST'] != "localhost")?'<a href="?page=admin&action=pull_git" type="button" class="btn btn-primary btn-lg btn-block">Git Pull To gitHub Origin master</a>':''?>
+					<a href="?page=admin&action=edit_config_app" type="button" class="btn btn-primary btn-lg btn-block">Edit Option Config</a>
+				</div><?
+			}
 		}?>
-		<div class="col-xs-6 col-xs-offset-3">
-			<?=($_SERVER['HTTP_HOST'] != "localhost")?'<a href="?page=admin&action=pull_git" type="button" class="btn btn-primary btn-lg btn-block">Git Pull To gitHub Origin master</a>':''?>
-			<a href="?page=admin&action=edit_config_app" type="button" class="btn btn-primary btn-lg btn-block">Edit Option Config</a>
-		</div>
 
-		
 		
 		<div style='font-size:13px; margin-bottom:0; color:red' class="col-xs-12 form-group <?php echo (isset($_SESSION['error']))?'has-error':''; ?>">
 			<?php echo (isset($_SESSION['error']))?'<label for="exampleInputPassword1">'.$_SESSION['error'].'</label>':''; ?>
-		</div>
 		</div>
 	</div>
 </div>
